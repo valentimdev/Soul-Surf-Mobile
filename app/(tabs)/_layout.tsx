1ac05eb44c58d1d6a0d1bdc52b7cbdd17bc9897f
lucasarrudab <silveiraalucas1@gmail.com>
2026-04-09 11:33:52 -0300

    Ocultado notificação e chat e movido teste do mapa para outra pasta para rodar o app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -52,6 +52,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="chat"
         options={{
+            href: null,
           title: 'Chat',
           tabBarIcon: ({ color, size }) => (
             <MessageCircle size={size} color={Colors.light.icon} />
@@ -61,6 +62,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="notifications"
         options={{
+            href: null,
           title: 'Notificações',
           tabBarIcon: ({ color, size }) => (
             <View>
